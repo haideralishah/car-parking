44f2edbb3713a71984b5955fab38edb61db28dd0
Haider <headeralishah@gmail.com>
2017-01-28 00:20:04 +0500

    User Admin Functionality done.

--- src/app/view-user-admin/view-user-admin.component.ts
+++ src/app/view-user-admin/view-user-admin.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+
+import {
+  RouterModule,
+  ActivatedRoute,
+  Router,
+  Routes
+} from '@angular/router';
+import { DataService } from '../data.service';
+declare var firebase: any;
 
 @Component({
   selector: 'app-view-user-admin',
@@ -7,7 +17,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewUserAdminComponent implements OnInit {
 
-  constructor() { }
+  users: any = [];
+  userID: any;
+  constructor(public router: Router, public route: ActivatedRoute, public dataService: DataService, public http: Http) {
+
+    firebase.database().ref('/users').on('child_added', (data) => {
+      let obj = data.val();
+      obj.id = data.key;
+      this.users.push(obj)
+      console.log(this.users, 'users');
+    })
+    this.userID = firebase.auth().currentUser.uid;
+  }
 
   ngOnInit() {
   }
