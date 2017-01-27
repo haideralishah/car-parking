5fbe9c99f9dc508b345d6f993bd09113cb252c86
Haider <headeralishah@gmail.com>
2017-01-27 21:18:11 +0500

    Booking and reservation done.

--- src/app/book-slot/book-slot.component.ts
+++ src/app/book-slot/book-slot.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+
 import {
   RouterModule,
   ActivatedRoute,
@@ -22,9 +24,8 @@ export class BookSlotComponent implements OnInit {
   obj: any = {};
   bookedSlot: any = [];
   bookingDetails: any;
-  constructor(public router: Router, public route: ActivatedRoute, public dataService: DataService) {
+  constructor(public router: Router, public route: ActivatedRoute, public dataService: DataService, public http: Http) {
     this.bookingDetails = this.dataService.getDetailsBookSlot();
-    console.log(this.bookingDetails, 'this.bookingDetails');
     if (this.bookingDetails.location == 'gj') {
       this.locationName = 'Gulistan Johar'
       this.bookingDetails.location = 'Gulistan Johar'
@@ -43,6 +44,7 @@ export class BookSlotComponent implements OnInit {
   }
 
   reservedSlot = [];
+
   updatedReservedSlot() {
     // console.log(this.bookedSlot, 'this.bookedSlotthis.bookedSlotthis.bookedSlot');
     // console.log(this.bookedSlot, 'this.bookedSlotthis.bookedSlotthis.bookedSlot');
@@ -50,24 +52,55 @@ export class BookSlotComponent implements OnInit {
     for (var i = 0; i < this.parkingSlot1.length; i++) {
       for (var j = 0; j < this.bookedSlot.length; j++) {
         // console.log(this.bookedSlot[j], 'this.bookedSlot[j]');
-        console.log(this.parkingSlot1[i].slotNumber, 'this.parkingSlot1[i] Outside Box');
         if (this.bookedSlot[j].slotNumber == this.parkingSlot1[i].slotNumber && this.bookedSlot[j].location == this.bookingDetails.location && (this.bookedSlot[j].startTimeInMilliSeconds >= this.bookingDetails.startTimeInMilliSeconds && this.bookedSlot[j].endTimeInMilliSeconds <= this.bookingDetails.endTimeInMilliSeconds) || (this.bookedSlot[j].endTimeInMilliSeconds >= this.bookingDetails.startTimeInMilliSeconds && this.bookedSlot[j].endTimeInMilliSeconds <= this.bookingDetails.endTimeInMilliSeconds)) {
-          console.log(this.bookedSlot[j].slotNumber, '**************************');
-          console.log(this.parkingSlot1[i].slotNumber, '**************************');
-
-          this.parkingSlot1[i].slotStatus = 'reserved';
+          if (this.reservedSlot.indexOf(this.bookedSlot[j].slotNumber) == -1) {
+            this.reservedSlot.push(this.bookedSlot[j].slotNumber);
+          }
         }
-
-        // if (this.bookedSlot[j].slotNumber == this.parkingSlot1[i].slotNumber && this.bookedSlot[j].location == this.bookingDetails.location && (this.bookedSlot[j].startTimeInMilliSeconds > this.bookingDetails.startTimeInMilliSeconds && this.bookedSlot[j].endTimeInMilliSeconds < this.bookingDetails.endTimeInMilliSeconds) || (this.bookedSlot[j].endTimeInMilliSeconds > this.bookingDetails.startTimeInMilliSeconds && this.bookedSlot[j].endTimeInMilliSeconds < this.bookingDetails.endTimeInMilliSeconds)) {
-        //   console.log(this.parkingSlot1[i].slotNumber, '**************************');
-        // }
       }
     }
-    // for (var i = 0; i < this.parkingSlot1.length; i++) {
-    //   if (this.bookedSlot.indexOf(this.parkingSlot1[i].slotNumber) != -1) {
-    //     this.parkingSlot1[i].slotStatus = 'reserved';
-    //   }
-    // }
+    for (var i = 0; i < this.parkingSlot1.length; i++) {
+      if (this.reservedSlot.indexOf(this.parkingSlot1[i].slotNumber) != -1) {
+        this.parkingSlot1[i].slotStatus = 'reserved';
+      }
+    }
+
+
+    for (var i = 0; i < this.parkingSlot2.length; i++) {
+      for (var j = 0; j < this.bookedSlot.length; j++) {
+        // console.log(this.bookedSlot[j], 'this.bookedSlot[j]');
+        if (this.bookedSlot[j].slotNumber == this.parkingSlot2[i].slotNumber && this.bookedSlot[j].location == this.bookingDetails.location && (this.bookedSlot[j].startTimeInMilliSeconds >= this.bookingDetails.startTimeInMilliSeconds && this.bookedSlot[j].endTimeInMilliSeconds <= this.bookingDetails.endTimeInMilliSeconds) || (this.bookedSlot[j].endTimeInMilliSeconds >= this.bookingDetails.startTimeInMilliSeconds && this.bookedSlot[j].endTimeInMilliSeconds <= this.bookingDetails.endTimeInMilliSeconds)) {
+          if (this.reservedSlot.indexOf(this.bookedSlot[j].slotNumber) == -1) {
+            this.reservedSlot.push(this.bookedSlot[j].slotNumber);
+          }
+        }
+      }
+    }
+    for (var i = 0; i < this.parkingSlot2.length; i++) {
+      if (this.reservedSlot.indexOf(this.parkingSlot2[i].slotNumber) != -1) {
+        this.parkingSlot2[i].slotStatus = 'reserved';
+      }
+    }
+
+
+    for (var i = 0; i < this.parkingSlot3.length; i++) {
+      for (var j = 0; j < this.bookedSlot.length; j++) {
+        // console.log(this.bookedSlot[j], 'this.bookedSlot[j]');
+        if (this.bookedSlot[j].slotNumber == this.parkingSlot3[i].slotNumber && this.bookedSlot[j].location == this.bookingDetails.location && (this.bookedSlot[j].startTimeInMilliSeconds >= this.bookingDetails.startTimeInMilliSeconds && this.bookedSlot[j].endTimeInMilliSeconds <= this.bookingDetails.endTimeInMilliSeconds) || (this.bookedSlot[j].endTimeInMilliSeconds >= this.bookingDetails.startTimeInMilliSeconds && this.bookedSlot[j].endTimeInMilliSeconds <= this.bookingDetails.endTimeInMilliSeconds)) {
+          if (this.reservedSlot.indexOf(this.bookedSlot[j].slotNumber) == -1) {
+            this.reservedSlot.push(this.bookedSlot[j].slotNumber);
+          }
+        }
+      }
+    }
+    for (var i = 0; i < this.parkingSlot3.length; i++) {
+      if (this.reservedSlot.indexOf(this.parkingSlot3[i].slotNumber) != -1) {
+        this.parkingSlot3[i].slotStatus = 'reserved';
+      }
+    }
+
+
+
     // for (var i = 0; i < this.parkingSlot2.length; i++) {
     //   if (this.bookedSlot.indexOf(this.parkingSlot2[i].slotNumber) != -1) {
     //     this.parkingSlot2[i].slotStatus = 'reserved';
@@ -108,12 +141,11 @@ export class BookSlotComponent implements OnInit {
   }
   warning: any = {};
   bookSlot(selectedSlot) {
-    console.log(selectedSlot, 'selectedSlot');
+    this.bookingDetails = {};
 
     if (selectedSlot.slotStatus == 'reserved') {
-      console.log(selectedSlot.slotStatus, 'selectedSlot.slotStatus');
       this.warning.status = true;
-      this.warning.message = 'Already Reserved Slot';
+      this.warning.message = 'Already reserved slot, but you can reserver it in some other time.';
       setTimeout(() => {
         this.warning.status = false;
         this.warning.message = ''
@@ -121,22 +153,39 @@ export class BookSlotComponent implements OnInit {
     }
     else {
 
-      console.log(this.bookingDetails);
       let that = this;
       this.bookingDetails.slotNumber = selectedSlot.slotNumber;
       var userId = firebase.auth().currentUser.uid;
       firebase.database().ref('/users/' + userId).once('value').then(function (snapshot) {
         var user = snapshot.val();
-        console.log(user, 'user');
         that.bookingDetails.email = user.email
         that.bookingDetails.mobilenumber = user.mobNumber
         that.bookingDetails.userName = user.userName
         that.bookingDetails.uid = userId
-        console.log(that.bookingDetails, 'bookingDetails');
         firebase.database().ref('bookedslots/').push(that.bookingDetails)
           .then((v) => {
             // console.log(v);
-            that.bookingDetails = {};
+
+            that.warning.status = true;
+            let opt: RequestOptions
+            let myHeaders: Headers = new Headers
+            myHeaders.set('from', 'Haider');
+            myHeaders.set('recipient', that.bookingDetails.email);
+            myHeaders.set('slotno', that.bookingDetails.slotNumber);
+            opt = new RequestOptions({
+              headers: myHeaders
+            })
+            let url = 'https://floating-castle-27628.herokuapp.com'
+            that.http.get(url + '/mailSend', opt)
+              // .map(res => res.json())
+              .subscribe(data => {
+                console.log(data);
+              })
+            that.warning.message = 'Your reservation confirmed, please check your email.';
+            setTimeout(() => {
+              that.warning.status = false;
+              that.warning.message = ''
+            }, 6000);
             // this.warningPublish = false;
 
           });;
